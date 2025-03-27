48d1d6df889e5def9d955f15b9de68bca6ecd9dd
Ashlesha Kadam <95738709+ashleshakadam@users.noreply.github.com>
2025-03-26 23:00:58 -0500

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,22 @@
+
+  <?php include 'header.php';?>
+  <!-- Bnr Header -->
+
+  <!-- Content --> 
+  <div id="content"> 
+    
+    <!-- Intro -->
+    <section class="p-t-b-150" style = "background-color:#f9f9f9">
+      <div class="logo" style = "display: flex; justify-content: center"> <img src="images/81075.jpg" alt="" style="height: 615px;"></a> </div>
+        
+      </div>
+    </section>
+    
+  
+
+    
+    
+  </div>
+  
+  <!-- Footer -->
+<?php include 'footer.php';?>
